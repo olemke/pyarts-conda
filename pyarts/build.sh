96c714d2c07bd357951f0b897df9b3722601d23b
Oliver Lemke <olemke@gmail.com>
2022-09-06 07:55:50 +0200

    Add OSX deployment target

--- pyarts/build.sh
+++ pyarts/build.sh
@@ -1,6 +1,11 @@
 mkdir -p build
 cd build
 
+if [[ $(uname) == "Darwin" ]]; then
+    export MACOSX_DEPLOYMENT_TARGET=11.0
+    export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
+fi
+
 cmake ${CMAKE_ARGS} -DCMAKE_PREFIX_PATH=${PREFIX} -DPython3_EXECUTABLE=${PREFIX}/bin/python3 -DENABLE_FORTRAN=1 -DENABLE_GUI=1 $SRC_DIR
 
 # Need to set this so executables that generate source code
