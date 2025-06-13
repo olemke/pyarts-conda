ca3ff40cf4ef520c0c07407c538e86347c948cee
Oliver Lemke <olemke@gmail.com>
2025-06-13 09:51:17 +0200

    Output auto_agenda_operators.cpp

--- pyarts/bld.bat
+++ pyarts/bld.bat
@@ -3,7 +3,8 @@ cd build
 
 cmake -G Ninja -DCMAKE_PREFIX_PATH=%PREFIX% -DPython_EXECUTABLE=%PYTHON% -DNO_OPENMP=1 %SRC_DIR%
 
-cmake --build . -j%CPU_COUNT% --target pyarts
+cmake --build . --verbose -j%CPU_COUNT% --target pyarts
+type %SRC_DIR%\build\src\auto_agenda_operators.cpp
 
 cd python
 python -m pip install .
