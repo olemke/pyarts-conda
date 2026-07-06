a7fa237bbee71ea0e81e8287a38cd8ad244826eb
Oliver Lemke <olemke@gmail.com>
2026-07-06 09:02:06 +0200

    Transition to rattler-build

--- pyarts/build.sh
+++ pyarts/build.sh
@@ -8,10 +8,6 @@ fi
 
 cmake -G Ninja ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${PREFIX} -DPython3_EXECUTABLE=${PREFIX}/bin/python3 $SRC_DIR
 
-# Need to set this so executables that generate source code
-# during build time (gen_auto_py) can find libGL
-# export LD_LIBRARY_PATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib
-
 cmake --build . -j${CPU_COUNT} --target pyarts
 
 cd python
